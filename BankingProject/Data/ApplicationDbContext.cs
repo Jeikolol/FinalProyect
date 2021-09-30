5846fd243b96045fda818b3f61c1276a313660ab
Jeikolol <53280663+Jeikolol@users.noreply.github.com>
2021-09-30 19:07:50 -0400

    First commit

---
+++ BankingProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BankingProject.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankingProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+
+       protected override void OnModelCreating(ModelBuilder builder)
+       {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>().ToTable("Users");
+       }
+
+       public DbSet<User> Users { get; set; }
+    }
+}
---
+++ BankingProject/Infrastructure/Service.cs
@@ -0,0 +1,15 @@
+﻿using BankingProject.Data;
+
+namespace BankingProject.Infrastructure
+{
+    public class Service : IService
+    {
+        protected readonly ApplicationDbContext Database;
+
+        public Service(ApplicationDbContext _Database)
+        {
+            Database = _Database;
+        }
+
+    }
+}
