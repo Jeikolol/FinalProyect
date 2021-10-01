10a6b93a3f3fb1997009b0db6a6faca17b683e5d
Dawin Rafael Perez Batista <dawin2020@hotmail.com>
2021-10-01 15:05:20 -0400

    Se aplico el gitignore

---
+++ BankingProject/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingProject.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ BankingProject/Services/SecurityService.cs
@@ -0,0 +1,49 @@
+﻿using BankingProject.Data;
+using BankingProject.Entities;
+using BankingProject.Infrastructure;
+using BankingProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankingProject.Services
+{
+    public class SecurityService : Service, ISecurityService
+    {
+        public SecurityService(ApplicationDbContext _Database) : base(_Database)
+        {
+        }
+
+        public async Task<User> CreateUserAsync(RegisterModel parameter)
+        {
+            var userToCreate = new User
+            {
+                UserName = parameter.UserName,
+                Password = parameter.Password
+            };
+
+            Database.Add(userToCreate);
+            await Database.SaveChangesAsync();
+
+            return userToCreate;
+        }
+
+        public async Task<User> Login(LoginModel param)
+        {
+            var user = await Database.Users
+                .Where(x => x.UserName == param.UserName &&
+                            x.Password == param.Password)
+                .FirstOrDefaultAsync();
+
+            return user;
+        }
+    }
+
+    public interface ISecurityService : IService
+    {
+        Task<User> CreateUserAsync(RegisterModel parameter);
+        Task<User> Login(LoginModel param);
+    }
+}
